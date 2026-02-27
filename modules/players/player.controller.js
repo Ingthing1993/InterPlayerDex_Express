71b51d8df171252923fa8edbcae473e9379f60e6
Ingthing1993 <benjamin-ing@live.co.uk>
2026-02-27 21:07:51 +0000

    Enhance updatePlayer method: improve player update logic with detailed error handling and structured player object creation in player.controller.

--- modules/players/player.controller.js
+++ modules/players/player.controller.js
@@ -11,8 +11,26 @@ const createPlayer = async (req, res) => {
 };
 
 const updatePlayer = async (req, res) => {
-    const player = await playerModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(player);
+    const player = {
+        name: req.body.name,
+        position: req.body.position,
+        birthdate: req.body.birthdate,
+        games_played: req.body.games_played,
+        goals: req.body.goals,
+        assists: req.body.assists,
+        joining_date: req.body.joining_date,
+        leaving_date: req.body.leaving_date,
+        image: req.body.image,
+    };
+    if (!player) {
+        return res.status(404).json({ message: 'Player not found' });
+    }
+    const updatedPlayer = await playerModel.findByIdAndUpdate(req.params.id, player, { new: true });
+    if (!updatedPlayer) {
+        return res.status(404).json({ message: 'Player not found' });
+    }
+    console.log(updatedPlayer);
+    res.json(updatedPlayer);
 };
 
 module.exports = {
