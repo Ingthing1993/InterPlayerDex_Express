47622c9e4570fb0f9341a6cf50038081a4bda38a
Ingthing1993 <benjamin-ing@live.co.uk>
2026-04-05 21:56:49 +0100

    Add team model: implement Mongoose schema for teams with required name field in team.model.js.

---
+++ modules/teams/team.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+const teamModel = mongoose.model('Team', teamSchema);
+
+module.exports = teamModel;
